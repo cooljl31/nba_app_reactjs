96adee2c39e56adc0a7a89905388ffe285e5325f
jim lesperance <contact@jimlesperance.com>
2018-05-17 17:03:56 +0200

    add widgets to home page

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import NewsSlider from '../widgets/NewsSlider/slider'
+import NewsList from '../widgets/NewsList/newsList'
 
 export const Home  = () => {
   return (
     <div>
-      Home
+      <NewsSlider
+        type="feature"
+        start={0}
+        amount={6}
+        dots={true}
+        settings={{
+          dots:false
+        }}
+      />
+      <NewsList
+      type="card"
+      loadmore={true}
+      start={3}
+      amount={0}
+      />
     </div>
   );
 };
