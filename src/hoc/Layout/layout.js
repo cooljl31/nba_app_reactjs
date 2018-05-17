fb0505973fd500a32c5cc4f52e0935048e86d9df
jim lesperance <contact@jimlesperance.com>
2018-05-17 17:01:59 +0200

    add footer and header to layout

--- src/hoc/Layout/layout.js
+++ src/hoc/Layout/layout.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import './layout.css';
 
+import Header from '../../components/Header/header'
+import Footer from '../../components/Footer/footer'
+
 class Layout extends Component {
   state = {
+    showNav:false
+  }
 
+  togglesSideNav = (action) => {
+    this.setState({
+      showNav:action
+    })
   }
 
   render() {
     return (
       <div>
+        <Header
+          showNav={this.state.showNav}
+          onHideNav={() => this.togglesSideNav(false)}
+          onOpenNav={() => this.togglesSideNav(true)}
+        />
         {this.props.children}
+        <Footer />
       </div>
     );
   }
