47047fb95c83f4421ae4ce8119e77f834c529f26
jim lesperance <contact@jimlesperance.com>
2018-05-18 12:58:49 +0200

    add new routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
 import NewsArticle from './components/Articles/News/Post/index';
+import VideosArticle from './components/Articles/videos/video/index'
+import NewsMain from './components/Articles/News/Main/index'
+import VideosMain from './components/Articles/videos/Mains/index'
 
 class Routes extends Component {
   render() {
@@ -10,7 +13,10 @@ class Routes extends Component {
       <Layout>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/articles/:id" component={NewsArticle} />
+          <Route path="/articles/:id" exac component={NewsArticle} />
+          <Route path="/videos/:id" exact component={VideosArticle} />
+          <Route path="/news" exact component={NewsMain} />
+          <Route path="/videos" exact component={VideosMain} />
         </Switch>
       </Layout>
     );
