f7014571088475779ccfa4504dbad9bf13985212
jim lesperance <contact@jimlesperance.com>
2018-05-18 13:49:13 +0200

    update readme file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 const FULL_YEAR = (new Date()).getFullYear()
-const URL = "http://localhost:3001"
+let URL = ''
+if (window.location.origin+window.location.pathname === 'http://localhost:3000/') {
+  URL = "http://localhost:3001"
+} else {
+  URL = 'http://192.168.0.9:3001'
+}
 export {
   FULL_YEAR,
   URL
 }
+console.log();
